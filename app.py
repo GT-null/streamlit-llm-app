f36b74e3bffd5835635b68de681cecb290ecfabd
GT-null <yamahoshi.ent@gmail.com>
2025-06-26 13:57:58 +0900

    fix: update app.py

--- app.py
+++ app.py
@@ -5,19 +5,19 @@ load_dotenv()
 import streamlit as st
 
 st.title("課題LMMアプリ: このアプリは専門家のアドバイスを提供します。")
-st.write("##### 専門家1: 医療事務の専門家")
-st.write("##### 専門家2: 医療看護の専門家")
+st.write("##### 専門家1: 医療事務員")
+st.write("##### 専門家2: 看護師")
 st.write("入力フォームにテキストを入力し、「実行」ボタンを押すことで専門家に質問することができます。")
 
 selected_expert = st.radio(
     "質問したい専門家を選択してください。",
-    ["医療事務の専門家", "医療看護の専門家"]
-)  
+    ["医療事務員", "看護師"]
+)
 
 st.divider()
 
 # 専門家に応じてプレースホルダーを変更
-if selected_expert == "医療事務の専門家":
+if selected_expert == "医療事務員":
     placeholder = "受付手順や事務手続き、医療費などについての質問に対応します。"
 else:
     placeholder = "診察や検査、治療などについての質問に対応します。"
